5ad750b1966c8e7968557e0b0e4b64e952b76a88
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-11 12:56:58 +0100

    rails partie cocktail done

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,35 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+serialized_drinks = open(url).read
+
+drinks = JSON.parse(serialized_drinks)
+ingredients = drinks['drinks']
+ingredient = []
+
+puts 'Creating 10 fake ingredients...'
+
+Ingredient.destroy_all
+
+# 10.times do
+#   ingredients.each do |drink|
+#     ingredient << drink['drinks']
+#       ingredient.each do |i|
+#      Ingredient.create(name: i)
+#       end
+#   end
+# end
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "soda")
+
+puts 'Finished!'
