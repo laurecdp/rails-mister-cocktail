ac456522567e1fc8246974cdc09539f807fff011
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-12 17:01:19 +0100

    rails-mister-cocktail front front end de show en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,6 @@ puts 'Creating fake ingredients...'
 50.times do
   ingredients.each do |ingredient|
     ingredient_name = ingredient.values
-    ingredient_name.flatten
     Ingredient.create(name: ingredient_name)
   end
 end
