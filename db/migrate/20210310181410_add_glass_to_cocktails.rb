b9ff34938b14fee9463a91da9a3ae09f2abf457f
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-10 19:16:22 +0100

    add info to cocktails

---
+++ db/migrate/20210310181410_add_glass_to_cocktails.rb
@@ -0,0 +1,5 @@
+class AddGlassToCocktails < ActiveRecord::Migration[6.0]
+  def change
+    add_column :cocktails, :glass, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,9 @@ Dose.destroy_all
 Ingredient.destroy_all
 
 ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-cocktail_url = 'https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json'
-
-serialized_cocktails = open(cocktail_url).read
 serialized_ingredients = open(ingredients_url).read
-
-list_of_cocktails = JSON.parse(serialized_cocktails)
 list_of_ingredients = JSON.parse(serialized_ingredients)
-
-puts serialized_ingredients
-
 ingredients = list_of_ingredients['drinks']
-
-
 puts 'Adding ingredients...'
 
 ingredients.each do |list_ingredients|
@@ -28,10 +18,18 @@ ingredients.each do |list_ingredients|
   end
 end
 
+puts "#{Ingredient.all.count} ingredients added"
+
+cocktail_url = 'https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json'
+serialized_cocktails = open(cocktail_url).read
+list_of_cocktails = JSON.parse(serialized_cocktails)
 puts 'Creating cocktails...'
 
 list_of_cocktails.each do |cocktail|
-  Cocktail.create(name: cocktail['name'])
+  Cocktail.create(name: cocktail['name'],
+                  preparation: cocktail['preparation'])
 end
+
+puts "#{Cocktail.all.count} cocktails added"
 
 puts 'Finished!'
